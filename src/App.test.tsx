81a13e046309ca7d6d8d65cfa9c03a679098de91
Harry Ho <harry.ho@192-168-1-102.tpgi.com.au>
2021-06-08 09:38:45 +1000

    ramper 1.0-RC1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Buy crypto/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import "./App.css";
+import OnramperWidget from "@onramper/widget";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "440px",
+        height: "595px",
+        boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.1)",
+        borderRadius: "10px",
+        margin: "auto"
+      }}
+    >
+      <OnramperWidget
+        defaultAmount={200}
+        API_KEY="pk_test_ass3gtLSWQpI11IWUZLJdrfyQhj7bTw_3xwLvhEvH6Q0"
+        filters={{
+          onlyCryptos: ["ETH"]
+        }}
+      />
+    </div>
+  );
+}
+
